a1fa4c6c5e0510498a4a08c7981cfbb689852e18
miku <149695804+mikuas@users.noreply.github.com>
2025-02-05 00:42:25 +0800

    Delete FluentWidgetModule/build/lib/FluentWidgets directory

--- FluentWidgetModule/build/lib/FluentWidgets/common/__init__.py
+++
@@ -1,5 +0,0 @@
-from .font import setFonts, setTextColor, setTextColors, getFont
-from .system_tray_icon import SystemTrayIcon
-from .tool_info import setToolTipInfo, setToolTipInfos
-from .fluent_icon import WinFluentIcon, Icon
-from .splitter import HorizontalSplitter, VerticalSplitter
